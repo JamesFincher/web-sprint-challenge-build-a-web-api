00fb9516b436b7d859739d3d0ea27a8b3389464d
heartandkarma <heartandkarma@gmail.com>
2022-05-12 15:37:21 -0700

    routes setup

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,39 @@
-// Write your "projects" router here!
+const express = require("express");
+const projects = require("./projects-model");
+const middleware = require("./projects-middleware");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  console.log("get / pinged");
+  res.end();
+});
+
+router.get("/:id", (req, res) => {
+  console.log("get /:id pinged");
+  res.end();
+});
+
+router.post("/", (req, res) => {
+  console.log("post / pinged");
+  console.log(req.body);
+  res.end();
+});
+
+router.put("/:id", (req, res) => {
+  console.log("put /:id pinged");
+  console.log(req.body);
+  res.end();
+});
+
+router.delete("/:id", (req, res) => {
+  console.log("delete /:id pinged");
+  res.end();
+});
+
+router.get("/:id/actions", (req, res) => {
+  console.log("get /:id/actions pinged");
+  res.end();
+});
+
+module.exports = router;
